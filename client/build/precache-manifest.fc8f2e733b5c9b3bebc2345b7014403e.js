ccdca858ae2969b86242cf523cb37ae1a5d1ca02
Paraskhilosiya <paraskhilosiyapkofficial@gmail.com>
2023-10-11 14:01:36 +0530

    fixed deployment problem

---
+++ client/build/precache-manifest.fc8f2e733b5c9b3bebc2345b7014403e.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "08d06d06c9881f4ba1c3e3e8a62defb6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "62026a9961254c5471b8",
+    "url": "/static/css/2.b9c16c38.chunk.css"
+  },
+  {
+    "revision": "ed104d7653430a32bc04",
+    "url": "/static/css/main.6ff654c3.chunk.css"
+  },
+  {
+    "revision": "62026a9961254c5471b8",
+    "url": "/static/js/2.67f97d3f.chunk.js"
+  },
+  {
+    "revision": "04a47b72c35e9a18177c0b883993b641",
+    "url": "/static/js/2.67f97d3f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "58253bf1cc062c695562",
+    "url": "/static/js/3.07eb052e.chunk.js"
+  },
+  {
+    "revision": "ed104d7653430a32bc04",
+    "url": "/static/js/main.b26810bf.chunk.js"
+  },
+  {
+    "revision": "4e0e34f265fae8f33b01b27ae29d9d6f",
+    "url": "/static/js/main.b26810bf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8fda7e7812e06249b148",
+    "url": "/static/js/runtime-main.60f6dbef.js"
+  }
+]);
